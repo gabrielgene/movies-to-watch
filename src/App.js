b86ee6114443dcd2f8846757974949f039adc259
Gabriel Genê <gabrielgene97@gmail.com>
2018-06-01 02:42:04 -0300

    Add app file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,82 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { withStyles } from "@material-ui/core/styles";
+import Topbar from './components/topbar';
+import Movies from './components/movies';
+import { moviesData } from './movies';
 
-class App extends Component {
+const styles = theme => ({
+  content: {
+    marginTop: theme.spacing.unit * 10,
+  },
+  root: {
+    margin: theme.spacing.unit * 2,
+  },
+  title: {
+    marginTop: theme.spacing.unit * 2,
+    marginLeft: theme.spacing.unit * 3,
+  },
+});
+
+class App extends React.Component {
+  state = {
+    checked: [],
+    data: [],
+  };
+
+  handleToggle = value => () => {
+    const { checked } = this.state;
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    currentIndex === -1 ?
+      newChecked.push(value) :
+      newChecked.splice(currentIndex, 1);
+
+    localStorage.setItem('data', JSON.stringify(newChecked));
+    this.setState({
+      checked: newChecked,
+    });
+  };
+
+  componentDidMount() {
+    const localData = localStorage.getItem('data');
+    if (localData === null) {
+      this.setState({
+        data: moviesData,
+      });
+    } else {
+      const checked = JSON.parse(localData);
+      this.setState({
+        data: moviesData,
+        checked,
+      });
+    }
+  }
+
   render() {
+    const { classes } = this.props;
+    const { data, checked } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Topbar />
+        <div className={classes.content}>
+          <div className={classes.root}>
+            {
+              data.map(m => (
+                <Movies
+                  key={m.link}
+                  {...m}
+                  onChange={this.handleToggle}
+                  checked={checked}
+                />
+              ))
+            }
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
