0f641ac05a9b59bf654a977932af26063e30a51e
Gabriel Genê <gabrielgene97@gmail.com>
2018-06-01 02:42:18 -0300

    Add movies file

---
+++ src/movies.js
@@ -0,0 +1,52 @@
+export const moviesData = [
+  {
+    name: 'Um dia de fúria (Falling Down)',
+    link: 'https://www.imdb.com/title/tt0106856/',
+    img: 'https://m.media-amazon.com/images/M/MV5BYjcxMzM3OWMtNmM3Yy00YzBkLTkxMmQtMDk4MmM3Y2Y4MDliL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg',
+  },
+  {
+    name: 'Stallone Cobra (Cobra) [Dublado]',
+    link: 'https://www.imdb.com/title/tt0090859/',
+    img: 'https://m.media-amazon.com/images/M/MV5BOWZhOWVmMzctY2RjMi00YzkxLTkwOGQtODA2MDk1MDg1ZWVlXkEyXkFqcGdeQXVyMDEwMjgxNg@@._V1_UX182_CR0,0,182,268_AL_.jpg',
+  },
+  {
+    name: 'Um tira da pesada (Beverly Hills Cop) [Dublado]',
+    link: 'https://www.imdb.com/title/tt0086960/',
+    img: 'https://m.media-amazon.com/images/M/MV5BN2MyZDE0YjAtNWFjYy00MWRlLTk3MTktMzY3ZDVhNTJkZTlmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_UY268_CR2,0,182,268_AL_.jpg',
+  },
+  {
+    name: 'Um tira da pesada 2 (Beverly Hills Cop II) [Dublado]',
+    link: 'https://www.imdb.com/title/tt0092644/',
+    img: 'https://m.media-amazon.com/images/M/MV5BNThlYWFhYmYtMzQzZS00ZmZjLWJjZjAtYWVhMmM4YTc1ZjhjXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_UY268_CR7,0,182,268_AL_.jpg',
+  },
+  {
+    name: 'Um tira da pesada 3 (Beverly Hills Cop III) [Dublado]',
+    link: 'https://www.imdb.com/title/tt0109254/',
+    img: 'https://m.media-amazon.com/images/M/MV5BOTdhMTU4MzMtYTVjMi00MTYzLTkxZDYtOWUwYjI0NDFjZTY1XkEyXkFqcGdeQXVyMDEwMjgxNg@@._V1_UX182_CR0,0,182,268_AL_.jpg',
+  },
+  {
+    name: 'De volta para o futuro (Back from the future)',
+    link: 'https://www.imdb.com/title/tt0088763/',
+    img: 'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg',
+  },
+  {
+    name: 'De volta para o futuro - parte 2 (Back from the Future Part II)',
+    link: 'https://www.imdb.com/title/tt0096874/',
+    img: 'https://m.media-amazon.com/images/M/MV5BZTMxMGM5MjItNDJhNy00MWI2LWJlZWMtOWFhMjI5ZTQwMWM3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg',
+  },
+  {
+    name: 'De volta para o futuro - parte 3 (Back from the Future Part III)',
+    link: 'https://www.imdb.com/title/tt0099088/',
+    img: 'https://m.media-amazon.com/images/M/MV5BYjhlMGYxNmMtOWFmMi00Y2M2LWE5NWYtZTdlMDRlMGEzMDA3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg',
+  },
+  {
+    name: 'V de Vingança (V for Vendetta)',
+    link: 'https://www.imdb.com/title/tt0434409/',
+    img: 'https://m.media-amazon.com/images/M/MV5BYzllMjJkODAtYjMwMi00YmNhLWFhYzAtZjZjODg5YzEwOGUwXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY268_CR0,0,182,268_AL_.jpg',
+  },
+  {
+    name: 'Jamaica abaixo de zero (Cool Runnings)',
+    link: 'https://www.imdb.com/title/tt0106611/',
+    img: 'https://m.media-amazon.com/images/M/MV5BMjMxMTQ3MzMwMV5BMl5BanBnXkFtZTgwNTYxNzYxMTE@._V1_UX182_CR0,0,182,268_AL_.jpg',
+  },
+];
